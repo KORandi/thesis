967cb897878425aa1f962d2aa0be22633c6bcac6
Konstantin Kozokar <konstantin@neotalogic.com>
2024-11-30 17:25:29 +0100

    Fix api url link

--- frontend/src/lib/services.ts
+++ frontend/src/lib/services.ts
@@ -1,10 +1,10 @@
 import { LlmConnectorData } from "@thesis/ckeditor5-llm-connector/interfaces/llm-connector-data.js";
 import { createAutocompleteService } from "./utils";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "";
 
 export const llamaService = (state: LlmConnectorData) =>
-  createAutocompleteService(state, `${API_URL}/llama/autocomplete`);
+  createAutocompleteService(state, `${API_URL}/api/llama/autocomplete`);
 
 export const gptService = (state: LlmConnectorData) =>
-  createAutocompleteService(state, `${API_URL}/gpt/autocomplete`);
+  createAutocompleteService(state, `${API_URL}/api/gpt/autocomplete`);
